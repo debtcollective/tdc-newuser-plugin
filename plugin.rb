7442167489f84f962f6dfdf8533d451020115e11
Shoshana Berleant <sberleant@gmail.com>
2018-04-10 17:57:29 -0700

    rename var; test user update triggers

--- plugin.rb
+++ plugin.rb
@@ -5,22 +5,33 @@
 register_asset "stylesheets/common/tdc-new-user.scss"
 
 after_initialize do
-  add_custom_user_fields()
+  add_user_custom_fields()
   set_up_event_triggers()
 end
 
-def add_custom_user_fields()
-  custom_user_fields = ['collectives', 'debt_amount', 'solidarity_how_can_you_help',
+def add_user_custom_fields()
+  user_custom_fields = ['collectives', 'debt_amount', 'solidarity_how_can_you_help',
     'solidarity_employment', 'solidarity_skills', 'phone']
 
-  custom_user_fields.each do |field_name|
+  user_custom_fields.each do |field_name|
     add_to_serializer(:user, :custom_fields) { user.custom_fields[field_name] }
     DiscoursePluginRegistry.serialized_current_user_fields << field_name
   end
+
+  # ugly hack so that the custom fields don't get filtered out in UsersController.create
+  # technically not very robust (if another plugin erases OPTION_ATTR, for example)
+  # but I'm reasonably confident it won't be a problem
+  ::UserUpdater::OPTION_ATTR.concat [ :custom_fields => [
+    :debt_amount,
+    :solidarity_how_can_you_help,
+    :solidarity_skills,
+    :solidarity_employment,
+    :phone,
+    :collectives => [],
+  ] ]
 end
 
 def set_up_event_triggers()
-
   DiscourseEvent.on(:user_created) do |user|
     assign_user_groups(user)
   end
@@ -31,7 +42,7 @@ def set_up_event_triggers()
 end
 
 def assign_user_groups(user)
-  # each collective has a correspnding user group (value) and category (key)
+  # each collective has a corresponding user group (value) and category (key)
   # see seed.js debtcollective/discourse-seed
 
   collectives = {
@@ -44,13 +55,20 @@ def assign_user_groups(user)
     'Court Fines and Fees Collective': 'court-fines-fees',
     'Medical Debt Collective': 'medical-debt',
     'Solidarity Bloc': 'solidarity-bloc' }
-
-  collectives.each do |category, group|
-    if user.custom_fields[collectives].include?(category)
+  collectives.each do |category, group_id|
+    group = Group.find(group_id)
+    if user.custom_fields['collectives'].include?(category)
       # the user is in the collective
       group.add(user)
     else # the user is not in the collective
       group.remove(user)
+    end
+
+    vals = {user: user.username, group_id: group_id}
+    if group.save
+      Rails.logger.debug("Successfully updated %<user>s's membership in %<group_id>s" % vals)
+    else
+      Rails.logger.error("Unsuccessful attempt to update %<user>s's membership in %<group_id>s" % vals)
     end
   end
 end
